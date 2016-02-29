b96d0d2fcba7c651a8766bfd7c40b859d8bc744c
Stephen Day <sd@unixtastic.com>
2016-02-29 22:59:50 +0100

    Bump bind version to 9.9.8-P3. Make with -j4.

--- setup.sh
+++ setup.sh
@@ -2,11 +2,11 @@
 apt-get -qq update && apt-get -qq upgrade
 apt-get -qq install gcc make wget libssl-dev
 cd /tmp
-wget -q -O bind-9.9.6-P1.tar.gz ftp://ftp.isc.org/isc/bind9/9.9.6-P1/bind-9.9.6-P1.tar.gz
-tar -xzf bind-9.9.6-P1.tar.gz
-cd /tmp/bind-9.9.6-P1
+wget -q -O bind-9.9.8-P3.tar.gz ftp://ftp.isc.org/isc/bind9/9.9.8-P3/bind-9.9.8-P3.tar.gz
+tar -xzf bind-9.9.8-P3.tar.gz
+cd /tmp/bind-9.9.8-P3
 ./configure --with-openssl --disable-linux-caps
-make
+make -j4
 groupadd bind && useradd -g bind -d /tmp -M -r -s /bin/false bind
 make install
 mkdir /var/log/bind && chown bind:bind /var/log/bind
