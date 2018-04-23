8a7d9c022e488f636bff81e43650aa5408c0210f
Ross Williams <ross@ross-williams.net>
2018-04-23 16:19:55 -0400

    Removed solar-poller/build path to make way for subtree

--- solar-poller/build/egauge-poller.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ -z "$SP_EGAUGE_URI" -o -z "$SP_ZABBIX_SERVER" ]; then
-	echo \$SP_EGAUGE_URI or \$SP_ZABBIX_SERVER is unset. Exiting.
-	exit 1;
-fi
-
-/usr/bin/curl -sS --fail --digest "$SP_EGAUGE_URI" | /usr/bin/xsltproc /root/egauge-to-zabbix.xsl - | /usr/bin/zabbix_sender --zabbix-server "$SP_ZABBIX_SERVER" --port "${SP_ZABBIX_PORT:-10051}" --with-timestamps -i -
